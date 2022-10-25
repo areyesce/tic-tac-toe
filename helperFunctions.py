51de619714a94324ff7d3bfbb2eff829d3158f72
areyesce <69359136+areyesce@users.noreply.github.com>
2022-10-25 16:37:51 -0700

    Update helperFunctions.py

--- helperFunctions.py
+++ helperFunctions.py
@@ -6,3 +6,11 @@ def printBoard(board):
     print('-----------')
     print(' ' + board[0] + ' | ' + board[1] + ' | ' + board[2])
     
+def chooseSymbol():
+    
+    symbol = ''
+    while not (symbol == 'X' or symbol == 'O'):
+        symbol = input('Welcome Tic-Tac-Toe Player.\nDo you want to be (X) or (O) ?')
+    if symbol == 'X':
+        return 'X','O'
+    else: return 'O', 'X'
