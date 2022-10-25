4e8ad3188293bda7d02b36acc04d892388b29d58
areyesce <69359136+areyesce@users.noreply.github.com>
2022-10-25 16:29:20 -0700

    Update helperFunctions.py

--- helperFunctions.py
+++ helperFunctions.py
@@ -1,1 +1,8 @@
-#
+def printBoard(board):
+    
+    print(' ' + board[6] + ' | ' + board[7] + ' | ' + board[8])
+    print('-----------')
+    print(' ' + board[3] + ' | ' + board[4] + ' | ' + board[5])
+    print('-----------')
+    print(' ' + board[0] + ' | ' + board[1] + ' | ' + board[2])
+    
