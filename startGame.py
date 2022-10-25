1700888519838c6c9ff8d9c909e8c6bc46ddcd53
areyesce <69359136+areyesce@users.noreply.github.com>
2022-10-25 16:38:10 -0700

    Update startGame.py

--- startGame.py
+++ startGame.py
@@ -3,5 +3,8 @@ def startGame():
     
     board = ['_']* 9
     printBoard(board)
+    player_symbol, bot_symbol = chooseSymbol()
+    print("PLAYER:",player_symbol)
+    print("BOT:",bot_symbol)
     
 startGame()
