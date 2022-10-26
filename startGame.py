94a479dff6118e738bb7d1bff4e796c4af9df18b
areyesce <69359136+areyesce@users.noreply.github.com>
2022-10-25 17:28:04 -0700

    Update startGame.py

--- startGame.py
+++ startGame.py
@@ -8,7 +8,7 @@ def startGame():
     print("PROGRAM RANDOMLY PICKS FIRST TURN ....\n")
     time.sleep(2)
     # current_turn = randomizeFirstTurn()
-    current_turn = 'PLAYER' #TODO: remove after testing
+    current_turn = 'BOT' #TODO: remove after testing
     print("%s GOES FIRST\n"%(current_turn))
     print("----- SPACES ARE MARKED AS SHOWN -----")
     printBoard(['0','1','2','3','4','5','6','7','8','9'])
@@ -32,9 +32,18 @@ def startGame():
                 else: current_turn = 'BOT'
         else:
             printBoard(board)
-            position = chooseSpot(board)
-                
+            position = randomSpot(board)
+            board[position] = bot_symbol
+            if checkWin(board,bot_symbol):
+                printBoard(board)
+                print('*** BOT WINS... BETTER LUCK NEXT TIME ***')
+                gameNotOver = False
+            else:
+                if boardIsFull(board):
+                    printBoard(board)
+                    print('*** UH-OH! BOARD IS FULL AND THERE IS NO WINNER ***')
+                    break
+                else: current_turn = 'PLAYER'
             
-            
-        gameNotOver = False #TODO: remove after testing
+        # gameNotOver = False #TODO: remove after testing
 startGame()
