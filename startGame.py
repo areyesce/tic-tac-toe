1d2383f850d56304cdb84b0532c8adbee25c31b6
areyesce <69359136+areyesce@users.noreply.github.com>
2022-10-26 17:17:40 -0700

    Update startGame.py

--- startGame.py
+++ startGame.py
@@ -1,5 +1,6 @@
 from helperFunctions import *
 import time
+
 def startGame():
     
     board = ['_']* 10
@@ -7,12 +8,14 @@ def startGame():
     player_symbol, bot_symbol = chooseSymbol()
     print("PROGRAM RANDOMLY PICKS FIRST TURN ....\n")
     time.sleep(2)
-    # current_turn = randomizeFirstTurn()
-    current_turn = 'BOT' #TODO: remove after testing
+    current_turn = randomizeFirstTurn()
+    # current_turn = 'PLAYER' #TODO: remove after testing
     print("%s GOES FIRST\n"%(current_turn))
+    time.sleep(1)
     print("----- SPACES ARE MARKED AS SHOWN -----")
     printBoard(['0','1','2','3','4','5','6','7','8','9'])
     print("--------------------------------------\n")
+    time.sleep(1)
     gameNotOver = True
     
     while gameNotOver:
@@ -30,11 +33,14 @@ def startGame():
                     print('*** UH-OH! BOARD IS FULL AND THERE IS NO WINNER ***')
                     break
                 else: 
-                    print("CURRENT TURN IS BOT\n")
+                    print("BOT'S TURN TO MAKE A MOVE...\n")
                     current_turn = 'BOT'
         else:
             printBoard(board)
-            position = randomSpot(board)
+            position = findBestMove(board,bot_symbol,player_symbol,bot_symbol)
+            # print("bestMove: ",bestMove)
+            # break
+            # position = randomSpot(board) #TODO: remove later, or give option
             board[position] = bot_symbol
             if checkWin(board,bot_symbol):
                 printBoard(board)
