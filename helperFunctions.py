c492fe88fe629fa89e1fe53671617f1dbfc0a29a
areyesce <69359136+areyesce@users.noreply.github.com>
2022-10-26 18:02:07 -0700

    Update helperFunctions.py

--- helperFunctions.py
+++ helperFunctions.py
@@ -31,6 +31,11 @@ def minimax(board,depth,isMax,player_symbol,bot_symbol):
                 board[i] = '_'
         return best
 
+""" FIND BEST MOVE FUNCTION 
+PURPOSE: iterate over available positions to find optimal move using minimax algorithm
+PARAMETERS: game board, bot symbol (only bot uses minimax), player and bot symbols
+RETURN: best move to make based on most optimal score found
+"""
 def findBestMove(board,symbol,player_symbol,bot_symbol):
     bestVal = -1000
     bestMove = 0
@@ -46,13 +51,13 @@ def findBestMove(board,symbol,player_symbol,bot_symbol):
             if (moveVal > bestVal):
                 bestMove = i
                 bestVal = moveVal
-    # print("The value of the best Move is :", bestVal)
-    # print()
     return bestMove
     
-    #go through each cell, calculate minimax function for empty cells
-    
-
+""" PRINT BOARD FUNCTION 
+PURPOSE: print current state of the game board
+PARAMETERS: game board
+RETURN: None
+"""
 def printBoard(board):
     print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
     print('-----------')
@@ -60,7 +65,12 @@ def printBoard(board):
     print('-----------')
     print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
     print()
-    
+
+""" CHOOSE SYMBOL FUNCTION 
+PURPOSE: prompt user to input which symbol to use to mark their positions
+PARAMETERS: None
+RETURN: ordered pair of user's chosen symbol and designated bot symbol
+"""
 def chooseSymbol():
     symbol = ''
     while not (symbol == 'X' or symbol == 'O'):
@@ -69,12 +79,22 @@ def chooseSymbol():
         return 'X','O'
     else: return 'O', 'X'
     
+""" FUNCTION TO RANDOMIZE FIRST TURN
+PURPOSE: randomly select which user will go first
+PARAMETERS: None
+RETURN: string indicating whether PLAYER or BOT goes first
+"""
 def randomizeFirstTurn():
     if random.randint(0, 1) == 0:
         return 'PLAYER'
     else:
         return 'BOT'
-        
+  
+""" CHOOSE SPOT FUNCTION 
+PURPOSE: prompt user to input which position to place their symbol
+PARAMETERS: game board
+RETURN: user's chosen position
+"""      
 def chooseSpot(board):
     position = 0
     while position not in [1,2,3,4,5,6,7,8,9] and spotIsEmpty(position,board):
@@ -83,23 +103,23 @@ def chooseSpot(board):
         while board[position] != '_':
             print("space %s is NOT EMPTY, try again\n"%(position))
             position = int(input("Choose an AVAILABLE position from 1-9: "))
-            # return position
     return position
-    
-def randomSpot(board):
-    position = 0
-    
-    while position not in [1,2,3,4,5,6,7,8,9] and spotIsEmpty(position,board):
-        position = random.choice([1,2,3,4,5,6,7,8,9])
-        while board[position] != '_':
-            position = random.choice([1,2,3,4,5,6,7,8,9])
-    return position
-
+ 
+""" CHECK IF SPOT IS EMPTY FUNCTION 
+PURPOSE: check whether given position on the game board is empty
+PARAMETERS: position, game board
+RETURN: TRUE if position is available, FALSE otherwise
+"""    
 def spotIsEmpty(position,board):
     if board[position] == '_':
         return True
     else: return False
-    
+   
+""" CHECK FOR A WIN FUNCTION 
+PURPOSE: checks whether given symbol has marked a win on the board
+PARAMETERS: symbol to check win for, game board
+RETURN: TRUE if symbol has marked a win, FALSE otherwise
+"""    
 def checkWin(board,symbol):
     return ((board[7] == symbol and board[8] == symbol and board[9] == symbol) or # across the top
     (board[4] == symbol and board[5] == symbol and board[6] == symbol) or # across the middle
@@ -109,7 +129,12 @@ def checkWin(board,symbol):
     (board[9] == symbol and board[6] == symbol and board[3] == symbol) or # down the right 
     (board[7] == symbol and board[5] == symbol and board[3] == symbol) or # diagonal
     (board[9] == symbol and board[5] == symbol and board[1] == symbol)) # diagonal
-    
+
+""" CHECK BOARD IS FULL FUNCTION 
+PURPOSE: iterate over every space to check if the board is full
+PARAMETERS: game board
+RETURN: TRUE if game board is full, FALSE otherwise
+"""       
 def boardIsFull(board):
     for i in range(1,10):
         if spotIsEmpty(i,board):
